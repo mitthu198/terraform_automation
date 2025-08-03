0bd50f14e11ac38ebd07e6087ccade968ad71503
mitthu198 <yadavmithilesh4@gmail.com>
2025-08-04 00:09:41 +0530

    testing the commit

--- Infra_Automation/module/vm_module/mydata.tf
+++ Infra_Automation/module/vm_module/mydata.tf
@@ -4,6 +4,8 @@ data "azurerm_subnet" "subnet_data" {
   resource_group_name  = var.rg_name
 }
 
+
+
 data "azurerm_public_ip" "pip_data" {
   name                = var.pip_name
   resource_group_name = var.rg_name
